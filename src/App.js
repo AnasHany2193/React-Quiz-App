29c8204c1801dddf8cffc6c88a59fd9e5fdf5e40
Anas Hany <anashany219@gmail.com>
2024-08-22 16:26:02 +0300

    🌐 Loaded questions from a fake API: Implemented API integration for quiz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // 'error', 'loading', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    default:
+      throw new Error("Unknown Action!");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
